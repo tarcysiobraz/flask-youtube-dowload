27cce59127bfce031759b04b1c1083bc2b8682b9
tarcysiobraz <tarcysio_braz@hotmail.com>
2023-07-15 16:16:01 -0300

    download playlist

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from flaskwebgui import FlaskUI
-from pytube import YouTube
+from pytube import Playlist, YouTube
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -24,27 +24,58 @@ def pytubeUi():
     form = YoutubeLinkForm()
     if form.validate_on_submit():
         link = form.url.data
-        video_id = extract_video_id(link)
-        if video_id:
-            folder = os.path.expanduser("~/Downloads")
-            try:
-                Download(video_id, folder)
-                flash(message="Download concluído!", category="success")
-            except Exception as e:
-                flash(
-                    category="danger",
-                    message=f"Ocorreu um erro durante o download: {str(e)}",
-                )
+        if is_playlist(link):
+            playlist_id = extract_playlist_id(link)
+            if playlist_id:
+                try:
+                    playlist = Playlist(
+                        f"https://www.youtube.com/playlist?list={playlist_id}"
+                    )
+                    folder = os.path.join(
+                        os.path.expanduser("~/Downloads"), playlist.title
+                    )
+                    os.makedirs(folder, exist_ok=True)
+                    download_playlist(playlist, folder)
+                    flash(category="success", message="Download da playlist concluído!")
+                except Exception as e:
+                    flash(
+                        category="danger",
+                        message=f"Ocorreu um erro durante o download da playlist: {str(e)}",
+                    )
+            else:
+                flash(category="danger", message="Link da playlist inválido")
         else:
-            flash(message="Link do YouTube inválido", category="danger")
+            video_id = extract_video_id(link)
+            if video_id:
+                try:
+                    video = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+                    folder = os.path.expanduser("~/Downloads")
+                    download_video(video, folder)
+                    flash(category="success", message="Download do vídeo concluído!")
+                except Exception as e:
+                    flash(
+                        category="danger",
+                        message=f"Ocorreu um erro durante o download do vídeo: {str(e)}",
+                    )
+            else:
+                flash("danger", "Link do YouTube inválido")
         return redirect(url_for("pytubeUi"))
     return render_template("index.html", form=form)
 
 
-@app.route("/download_complete")
-def download_complete():
-    flash("success", "Download concluído!")
-    return redirect(url_for("pytubeUi"))
+def is_playlist(url):
+    parsed_url = urlparse(url)
+    return parsed_url.path == "/playlist"
+
+
+def extract_playlist_id(url):
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    playlist_id = query_params.get("list")
+    if playlist_id:
+        return playlist_id[0]
+    else:
+        return None
 
 
 def extract_video_id(url):
@@ -57,10 +88,23 @@ def extract_video_id(url):
         return None
 
 
-def Download(video_id, folder):
-    youtubeObject = YouTube(f"https://www.youtube.com/watch?v={video_id}")
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
-    youtubeObject.download(output_path=folder)
+def download_playlist(playlist, folder):
+    for video in playlist.videos:
+        try:
+            youtube_object = video.streams.get_highest_resolution()
+            file_path = os.path.join(folder, video.title + ".mp4")
+            youtube_object.download(output_path=file_path)
+        except Exception as e:
+            print(f"Erro ao baixar o vídeo: {str(e)}")
+
+
+def download_video(video, folder):
+    try:
+        youtube_object = video.streams.get_highest_resolution()
+        file_path = os.path.join(folder, video.title + ".mp4")
+        youtube_object.download(output_path=file_path)
+    except Exception as e:
+        print(f"Erro ao baixar o vídeo: {str(e)}")
 
 
 if __name__ == "__main__":
